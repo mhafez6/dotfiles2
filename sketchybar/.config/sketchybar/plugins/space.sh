0086910483684162f8a77ab6ac6366d5cd06b8d5
Mohammad Hafez <mohammedxhafiz@gmail.com>
2026-06-21 10:46:11 -0700

    redesign: floating island bar with pill backgrounds

--- sketchybar/.config/sketchybar/plugins/space.sh
+++ sketchybar/.config/sketchybar/plugins/space.sh
@@ -2,7 +2,11 @@
 source "$HOME/.local/share/tinted-theming/tinty/tinted-sketchybar-themes-file.sh" 2>/dev/null
 
 if [ "$SELECTED" = true ]; then
-  sketchybar --animate tanh 10 --set "$NAME" icon.color=$SBAR_YELLOW
+  sketchybar --animate tanh 10 --set "$NAME" \
+    icon.color=$SBAR_GREEN \
+    background.border_color=$SBAR_GREEN
 else
-  sketchybar --animate tanh 10 --set "$NAME" icon.color=$SBAR_DIM
+  sketchybar --animate tanh 10 --set "$NAME" \
+    icon.color=$SBAR_FG \
+    background.border_color=$SBAR_BG_ALT
 fi
